fc6a1cec9d9acac4c9599edc0697329ed7e3781f
Tina <tina.khokhryakova@idapgroup.com>
2018-04-04 13:09:59 +0300

    refactoring server

--- server.js
+++ server.js
@@ -2,26 +2,26 @@
 
 // main const variable
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-const logger = require('./lib/logger')(module);
 const app = express();
 const router = express.Router();
+const port = process.env.PORT || 8081;
+
+require('./lib/db');
 
 // requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('json spaces', 40);
-
 // routes
-require('./routes')(router, {});
+require('./routes')(router);
 app.use('/api/v1', router);
 
 // error handling
 require('./lib/errorHandler')(app);
 
+
 // listen
-app.listen(8081, () => {
-    console.log('Express server listening on port 8081');
+app.listen(port, () => {
+    console.log('Express server listening on port ' + port);
 });
